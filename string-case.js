eb480626ba63b998d2544e5750b6e186c8df17df
xamanDawud <ahmeddaudking100@gmail.com>
2022-08-07 13:25:57 +0600

    string case Compare Explored

---
+++ string-case.js
@@ -0,0 +1,23 @@
+let power = "Who have the power of bringing my tower";
+console.log(power.length);
+
+// This is case sensitive compare
+let userName = "bts armY";
+let userInput = "bts army";
+
+if (userName === userInput) {
+    console.log("This user is Valid");
+} else {
+    console.log("This user isnot valid");
+}
+
+// another useful compare
+
+let userSave = "Anayan Panday";
+let userGave = "anayan panday";
+
+if (userSave.toLowerCase() === userGave.toLowerCase()) {
+    console.log("He can access the Database");
+} else {
+    console.log("He ever never access the Database");
+}

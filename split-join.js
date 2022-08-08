ef527aa86f92022f58490aa48fb13a4bfc1e8ee6
xamanDawud <ahmeddaudking100@gmail.com>
2022-08-08 08:16:44 +0600

    split types explored

---
+++ split-join.js
@@ -0,0 +1,30 @@
+let lyrics =
+    "Sada Sada kala Kala , Rong Jomese Sada Kala , Tumi Bondu Kala Pakhi Ami Jeno Ki";
+let parts = lyrics.split(" ");
+let sentense = lyrics.split(",");
+let character = lyrics.split("");
+console.log(character);
+
+//
+
+const cutCut = lyrics.slice(5, 10);
+console.log(cutCut);
+
+const alsoCut = lyrics.substring(14, 20);
+console.log(alsoCut);
+
+//
+
+let cleanDast = "      Hello xaman Mama! whats up";
+let clean = cleanDast.trim();
+console.log(clean);
+//
+
+let poem = [
+    "Badam Badam Mamam Kacha Badam",
+    "amar kache naiko mama baja badam ",
+    "amar kache ache sudu baja badam",
+];
+
+let addin = poem.join("--");
+console.log(addin);
---
+++ temp.js
@@ -0,0 +1,5 @@
+let arr = [23, 54, 67, 34, 22];
+let tallest = [];
+for (let i = 0; i < arr.length; i++) {
+    console.log(Math.max(i));
+}

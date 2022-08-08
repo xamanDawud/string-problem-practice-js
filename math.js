10090b8aea78bd829cf2f767af6a126b5c68d39c
xamanDawud <ahmeddaudking100@gmail.com>
2022-08-08 08:41:53 +0600

    math explore done

---
+++ math.js
@@ -0,0 +1,22 @@
+let ageMe = 17;
+let ageYou = 20;
+// let sub = ageMe - ageYou; // -3
+let sub = Math.abs(ageMe - ageYou);
+console.log(sub);
+
+if (sub < 5) {
+    console.log("We can Friend");
+} else {
+    console.log("Tumi Tumar Rasta Deko babu");
+}
+
+//-------------------------------------------------------------//
+
+let number = 235.643524;
+console.log(Math.round(number));
+console.log(Math.ceil(number));
+console.log(Math.floor(number));
+console.log(Math.random());
+
+let random = Math.random() * 20;
+console.log(Math.round(random));
